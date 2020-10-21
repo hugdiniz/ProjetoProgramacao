b07facdda1eb6eb16b5491e02d6d7fb460e4bcd3
Hugo Diniz Rebelo <hugo.rebelo@radixeng.com.br>
2020-10-21 20:42:39 -0300

    add orientação objeto

---
+++ src/OrientaçãoObjeto/OrientaçãoObjeto/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrientaçãoObjeto
+{
+    class Carro
+    {
+        public String cor;
+        public int ano;
+        public Carro(string corEntrada,int anoCarro)
+        {
+            cor = corEntrada;
+            ano = anoCarro;
+        }
+
+        public Boolean pagaIpva()
+        {
+            if(ano < 2010)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ src/OrientaçãoObjeto/OrientaçãoObjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OrientaçãoObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro("preto",2009);
+
+            Console.WriteLine(c.pagaIpva());
+        }
+    }
+}
